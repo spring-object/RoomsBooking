2a9f6b5c3e241ec37188e491fd78272b5122ea61
skyline <skyline@skyline-PC>
2019-06-20 10:04:26 +0800

    修改前端文件位置

--- src/main/java/com/booking/controller/UserController.java
+++ src/main/java/com/booking/controller/UserController.java
@@ -17,6 +17,43 @@ public class UserController {
 	@GetMapping("/test")
 	public @ResponseBody String test() {
 		userService.test();
-		return "index";
+		return "test";
 	}
+	@GetMapping("/")
+	public String index() {
+		return "user/index";
+	}
+	@GetMapping("/info/show")
+	public String showInfo() {
+		return "user/show_info";
+	}
+	@GetMapping("/info/change")
+	public String changeInfo() {
+		return "user/change_info";
+	}
+	@GetMapping("/password/change")
+	public String changePassword() {
+		return "user/change_password";
+	}
+	@GetMapping("/order/show")
+	public String showOrder() {
+		return "user/order_list";
+	}
+	@GetMapping("/order/sta")
+	public String staOrder() {
+		return "user/order_sta";
+	}
+	@GetMapping("/login")
+	public String login() {
+		return "user/sign_in";
+	}
+	@GetMapping("/logout")
+	public String logout() {
+		return "user/sign_in";
+	}
+	@GetMapping("/logup")
+	public String logup() {
+		return "user/sign_up";
+	}
+
 }
